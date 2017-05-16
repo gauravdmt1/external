34bdcb51a7e1bdc438a53709deebf38222390b39
Gaurav Sharma <gauravdmt1@gmail.com>
2017-05-16 16:42:10 +0530

    Created substractTwoNumbers.java

---
+++ substractTwoNumbers.java
@@ -0,0 +1,11 @@
+class substactTwoNumbers
+{
+  static int substract(int a,int b)
+  {
+    return a - b;
+  }
+  public static void main(String args[])
+  {
+    System.out.println("Substraction function : " + substract(3,2));
+  }
+}
