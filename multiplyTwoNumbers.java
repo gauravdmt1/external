b87f8b7f0d026c038405e519cfb9945e4c6d23e5
Gaurav Sharma <gauravdmt1@gmail.com>
2017-05-16 16:43:19 +0530

    Created multiplyTwoNumbers.java

---
+++ multiplyTwoNumbers.java
@@ -0,0 +1,11 @@
+class multiplyTwoNumbers
+{
+  static int multiply(int a,int b)
+  {
+    return a * b;
+  }
+  public static void main(String args[])
+  {
+    System.out.println("Multiplication function : " + multiply(3,4));
+  }
+}
