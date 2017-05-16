e979133220b4d55f2152a5d36d7311c0169a0415
Gaurav Sharma <gauravdmt1@gmail.com>
2017-05-16 16:36:46 +0530

    Created addTwoNumbers.java

---
+++ addTwoNumbers.java
@@ -0,0 +1,11 @@
+class AddNumbers
+{
+  static int add(int a,int b)
+  {
+    return a + b;
+  }
+  public static void main(String args[])
+  {
+    System.out.println("Sum of numbers : " + add(2,3));
+  }
+}
