94a973af157882ce2dc8eafc0446a0ef3617a6d9
Gaurav Sharma <gauravdmt1@gmail.com>
2017-05-16 16:49:22 +0530

    Created addMultiplyThreeNumbers.java file.

---
+++ addMultiplyThreeNumbers.java
@@ -0,0 +1,16 @@
+class addMultiplyThreeNumbers
+{
+  static int add(int a,int b,int c)
+  {
+    return a + b + c;
+  }
+  static int multiply(int a,int b,int c)
+  {
+    return a * b * c;
+  }
+  public static void main(String args[])
+  {
+    System.out.println("Addition function : " + add(2,3,4));
+    System.out.println("Multiplication function : " + multiply(2,3,4));
+  }
+}
